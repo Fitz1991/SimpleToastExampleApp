59086c408919b85f5dd801f3410886ff4f5019af
Dimka Potr <dimkapotr@outlook.com>
2020-05-26 17:49:18 +0300

    changed param

--- simpletoast/src/main/java/ru/fitz/simpletoast/SimpleToast.kt
+++ simpletoast/src/main/java/ru/fitz/simpletoast/SimpleToast.kt
@@ -3,8 +3,8 @@ package ru.fitz.simpletoast
 import android.content.Context
 
 class SimpleToast(private val context: Context) {
-    fun showText(text: String, abstractToast: AbstractToast = ShortToast) {
+    fun showText(message: String, abstractToast: AbstractToast = ShortToast) {
         abstractToast.context = context
-        abstractToast.showText(text)
+        abstractToast.showText(message)
     }
 }
